e4465c05267e7d4cfab8577e8cee882dcd1eae64
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2025-02-12 22:37:14 -0500

    beginning of exercies

--- part1a/introdemo/src/App.jsx
+++ part1a/introdemo/src/App.jsx
@@ -2,12 +2,20 @@
 import './App.css'
 
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
 }
 
 const App = () => {
@@ -16,11 +24,12 @@ const age = 10
 
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Waskar"/>
       <Hello name={name} age={age}/>
-    </div>
+      <Footer />
+    </>
   )
 }
 
