a275a49e4f3f088d1ddf4fb1b3e9149f1501f881
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2025-02-14 16:02:56 -0500

    2/14

--- part1d/unicafe/src/App.jsx
+++ part1d/unicafe/src/App.jsx
@@ -2,27 +2,45 @@
 import { useState } from 'react'
 
 
-const Heading = ({title}) => {
-  return(
+const Heading = ({ title }) => {
+  return (
     <>
-    <h1>{title}</h1>
+      <h1>{title}</h1>
     </>
   )
 }
 
-const Button = ({onClick, text}) => {
+const Button = ({ onClick, text }) => {
   return (
     <>
-    <button onClick={onClick}>{text}</button>
+      <button onClick={onClick}>{text}</button>
     </>
   )
 }
 
-const Content = ({text, value}) => {
+const Content = ({ text, value }) => {
   return (
     <>
-    <p>{text} {value}</p>
+      <p>{text} {value}</p>
     </>
+  )
+}
+
+const Statistics = ({good, bad, neutral, all, average, percentage}) => {
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <Heading title="Statistics" />
+      <Content text="good" value={good} />
+      <Content text="neutral" value={neutral} />
+      <Content text="bad" value={bad} />
+      <Content text="all" value={all} />
+      <Content text="average" value={average} />
+      <Content text="positive feedback" value={percentage} />
+    </div>
   )
 
 }
@@ -36,64 +54,31 @@ const App = () => {
   const [average, setAverage] = useState(0)
   const [percentage, setPercentage] = useState(0)
 
-  const makeAverage = () => {
-    if (all > 0) {
-      const newAverage = (good - bad) / all
-      setAverage(newAverage)
-      console.log(newAverage, 'new')
-    } else {
-      setAverage(0)
-    }
-  }
-  const makePercentage = () => {
-    if (all > 0) {
-      const newPercantage = ((good / all))
-      setPercentage(parseFloat(newPercantage))
-      console.log(newPercantage, 'new')
-    } else {
-      setPercentage(0)
-    }
-  }
-  const setTotal = () => {
+  const handleFeedback = (type) => {
+    const newGood = type === 'good' ? good + 1 : good
+    const newNeutral = type === 'neutral' ? neutral + 1 : neutral
+    const newBad = type === 'bad' ? bad + 1 : bad
     const newAll = all + 1
+
+    const newAverage = (newGood - newBad) / newAll
+    const newPercentage = (newGood / newAll) / 100
+
+    setGood(newGood)
+    setNeutral(newNeutral)
+    setBad(newBad)
     setAll(newAll)
-    console.log(newAll, 'all')
+    setAverage(newAverage)
+    setPercentage(newPercentage)
   }
 
-  const raiseGood = () => {
-    const newGood = good + 1
-    setGood(newGood)
-    setTotal()
-    makeAverage()
-    makePercentage()
-  }
-  const raiseNeutral = () => {
-    const newNeutral = neutral + 1
-    setNeutral(newNeutral)
-    setTotal()
-    makeAverage()
-  }
-  const raiseBad = () => {
-    const newBad = bad + 1
-    setBad(newBad)
-    setTotal()
-    makeAverage()
-  }
-  
 
   return (
     <div>
-      <Heading title='Give Feedback'/>
-      <Button onClick={raiseGood} text='good'/>
-      <Button onClick={raiseNeutral} text='neutral'/>
-      <Button onClick={raiseBad} text='bad'/>
-      <Heading title='Statistics' />
-      <Content text='good' value={good}/>
-      <Content text='neutral' value={neutral}/>
-      <Content text='bad' value={bad}/>
-      <Content text='all' value={all}/>
-      <Content text='average' value={average}/>
-      <Content text='positive' value={percentage}/>
+      <Heading title='Give Feedback' />
+      <Button onClick={() => handleFeedback('good')} text='good' />
+      <Button onClick={() => handleFeedback('neutral')} text='neutral' />
+      <Button onClick={() => handleFeedback('bad')} text='bad' />
+    <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} percentage={percentage} />
     </div>
   )
 }
